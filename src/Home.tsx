45056187f7e9ed8e0a08811c60d51316e402e656
Muhammad Tariq <muhammad64tariq@gmail.com>
2021-01-09 15:13:23 +0500

    updated

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import MissionDetails from './components/MissionInfo/MissionDetails';
 import {Navbar,Nav,Button} from 'react-bootstrap'
 import logo1 from './space-img/logo1.png'
@@ -18,7 +17,7 @@ export default function Home() {
    }, [])
   return (
     <div className="main">
-      <BrowserRouter> 
+ 
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light" data-aos="fade-down">
   <Navbar.Brand data-aos="zoom-in">
     <img src={logo1} width={40} height={40} alt="Loading..."/>
@@ -27,19 +26,13 @@ export default function Home() {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
     <Nav >
-      <Nav.Link >
-        <Link to="/"><Button variant="light">Home</Button></Link>
-        </Nav.Link>
-      <Nav.Link >
-      <Link to="/missions"><Button variant="light">Missions</Button></Link>
-      </Nav.Link>
+    
     </Nav>
   </Navbar.Collapse>
-</Navbar>
-        
+</Navbar>      
         <MissionListContainer handleIdChange={handleIdChange}/>
         <MissionDetails id={id}/>
-      </BrowserRouter>
+    
       <div className="txt">
 <h5 className="clr">
   <ReactTypingEffect
@@ -50,5 +43,66 @@ export default function Home() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+// import React, { useEffect } from 'react';
+// import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+// import MissionDetails from './components/MissionInfo/MissionDetails';
+// import {Navbar,Nav,Button} from 'react-bootstrap'
+// import logo1 from './space-img/logo1.png'
+// import logo from './space-img/logo.png'
+// import ReactTypingEffect from 'react-typing-effect';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css'; 
+// import MissionListContainer from './components/Mission';
+// export default function Home() {
+//   const [id, setId] = React.useState(0);
+//   const handleIdChange = React.useCallback(newId => {
+//     setId(newId);
+//   }, []);
+//   useEffect(() => {
+//     AOS.init({duration:1000});
+//    }, [])
+//   return (
+//     <div className="main">
+//       <BrowserRouter> 
+//       <Navbar collapseOnSelect expand="lg" bg="light" variant="light" data-aos="fade-down">
+//   <Navbar.Brand data-aos="zoom-in">
+//     <img src={logo1} width={40} height={40} alt="Loading..."/>
+//     <img src={logo} width={100} height={40} alt="Loading..."/>  
+//   </Navbar.Brand>
+//   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//   <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+//     <Nav >
+//       <Nav.Link >
+//         <Link to="/"><Button variant="light">Home</Button></Link>
+//         </Nav.Link>
+//       <Nav.Link >
+//       <Link to="/missions"><Button variant="light">Missions</Button></Link>
+//       </Nav.Link>
+//     </Nav>
+//   </Navbar.Collapse>
+// </Navbar>
+//         <Routes>
+//           <Route path='/missions' element={<MissionListContainer handleIdChange={handleIdChange}/>} />
+//           <Route path='/missions/flight' element={<MissionDetails id={id}/>} />
+//         </Routes>
+//       </BrowserRouter>
+//       <div className="txt">
+// <h5 className="clr">
+//   <ReactTypingEffect
+//   text="“SpaceX is in the process of creating the greatest environmental catastrophe I have ever witnessed.”"
+//  />
+//   </h5>
+// </div>
+//     </div>
+//   );
+// }
 
 
